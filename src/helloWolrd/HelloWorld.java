b0039b7076d05495eb116fc02f3a37ff13b92720
martin.medek <martin.medek@AMS-GYM-EDV1-13.gym.ams.local>
2019-11-04 18:30:34 +0100

    mehr

--- src/helloWolrd/HelloWorld.java
+++ src/helloWolrd/HelloWorld.java
@@ -5,9 +5,17 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hurensohn.");
+		System.out.println("Hurensohn");
+		System.out.print("Hallo du!");
+		System.out.println(120);
 		
+		System.out.println(8.0/3.0);
 
+		System.out.println("1 + 1="+(1+1));
+		
+		System.out.println(8/3);
+		System.out.println(8D/3);
+		System.out.println(8/3F);
 	}
 
 }
