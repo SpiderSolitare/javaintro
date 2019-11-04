bf26944abd019399d3f83dbccdff9905d325325f
martin.medek <martin.medek@AMS-GYM-EDV1-13.gym.ams.local>
2019-11-04 18:49:38 +0100

    Variablen

---
+++ src/helloWolrd/VariablenBasics.java
@@ -0,0 +1,32 @@
+package helloWolrd;
+
+public class VariablenBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean wahr_oder_falsch;
+		wahr_oder_falsch = true;
+		System.out.println(wahr_oder_falsch);
+		
+		wahr_oder_falsch = false;
+		System.out.println(wahr_oder_falsch);
+		
+		// short - 2Byte
+		
+		short gkz = 120;
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		
+		//byte
+		
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		
+		//int - 4 byte
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+	}
+
+}
