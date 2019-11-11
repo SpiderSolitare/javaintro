3dafe96218bcb92171531c906fb2a861227cee1e
martin.medek <martin.medek@AMS-GYM-EDV1-13.gym.ams.local>
2019-11-11 18:47:04 +0100

    Kleines 1x1

---
+++ src/javaBasics/Kleines1x1.java
@@ -0,0 +1,14 @@
+package javaBasics;
+
+public class Kleines1x1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+
+		for (int i = 1; i < 11; i = i + 1)
+			for (int j = 1; j < 11; j = j + 1)
+				System.out.println(j + "*" + i + "=" + (i+j));
+		
+		}
+	}
+
