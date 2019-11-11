164fdbd854f32da3aff055677432a40ee807ea75
martin.medek <martin.medek@AMS-GYM-EDV1-13.gym.ams.local>
2019-11-11 18:19:55 +0100

    for-Schleife

---
+++ src/javaBasics/SchleifenBasics.java
@@ -0,0 +1,31 @@
+package javaBasics;
+
+public class SchleifenBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// fot Schleife
+		for (int i = 0; i < 5; i = i + 1) 
+			System.out.println(i);
+		
+		//
+		int j = 1;
+		for (;j<10;) {
+			
+			System.out.println(j);
+			j = j + 1;
+		}
+		
+		//endlos Schleife
+		
+		/*for (;3<4;) {
+		*	
+		*	System.out.println("Verkackt");
+		*	
+		} 
+		*/
+		
+	}
+
+}
