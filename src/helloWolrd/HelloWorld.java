aaec165e695375deb6154e464eb7979d800a9c3b
martin.medek <martin.medek@AMS-GYM-EDV1-13.gym.ams.local>
2019-11-11 17:43:43 +0100

    fertig gemacht

--- src/helloWolrd/HelloWorld.java
+++ src/helloWolrd/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hurensohn");
+		System.out.println("123");
 		System.out.print("Hallo du!");
 		System.out.println(120);
 		
---
+++ src/javaBasics/VariablenBasics.java
@@ -0,0 +1,58 @@
+package javaBasics;
+
+public class VariablenBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean wahr_oder_falsch;
+		wahr_oder_falsch = true;
+		System.out.println(wahr_oder_falsch);
+		
+		wahr_oder_falsch = false;
+		System.out.println(wahr_oder_falsch);
+		System.out.println("");
+		
+		// short - 2Byte
+		
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println("");
+		
+		//byte
+		
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println("");
+		
+		//int - 4 byte
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println("");
+		
+		// long
+		long grosse_zahl = 123456789;
+		System.out.println(grosse_zahl);
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);
+		System.out.println("");
+		
+		// float
+		float kkz = 123.321F; // F ... Float
+		System.out.println(kkz);
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Float.MIN_VALUE);
+		System.out.println("");
+		
+		// double
+		double gkz1 = 123456789.987654321;
+		System.out.println(gkz1);
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Double.MIN_VALUE);
+		System.out.println("");
+				
+	}
+	
+
+}
