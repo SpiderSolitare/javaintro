75ea94fe2d22f9414f7dacd77620a46ad7853896
martin.medek <martin.medek@AMS-GYM-EDV1-13.gym.ams.local>
2019-11-18 18:14:07 +0100

    EvenNumbers

---
+++ src/aufgaben/EvenNumbers.java
@@ -0,0 +1,18 @@
+package aufgaben;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 0; i < 101; i = i +1) {
+			
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+			
+		}
+		
+	}
+
+}
