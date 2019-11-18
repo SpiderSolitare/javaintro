6c820c244f89fa887c1f7cee95c46219e99b84be
martin.medek <martin.medek@AMS-GYM-EDV1-13.gym.ams.local>
2019-11-18 17:15:58 +0100

    fixes

--- src/javaBasics/Kleines1x1.java
+++ src/javaBasics/Kleines1x1.java
@@ -7,7 +7,7 @@ public class Kleines1x1 {
 
 		for (int i = 1; i < 11; i = i + 1)
 			for (int j = 1; j < 11; j = j + 1)
-				System.out.println(j + "*" + i + "=" + (i+j));
+				System.out.println(j + "*" + i + "=" + (i*j));
 		
 		}
 	}
