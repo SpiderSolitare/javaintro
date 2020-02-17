05cfea5248d8417371297e192c73f555d85d483a
martin.medek <martin.medek@AMS-GYM-EDV1-13.gym.ams.local>
2020-02-17 17:43:24 +0100

    Namen Sort fertig

--- src/aufgaben/NamenSortieren.java
+++ src/aufgaben/NamenSortieren.java
@@ -1,24 +1,33 @@
 package aufgaben;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class NamenSortieren {
+
 	public static void main(String[] args) {
+
 		ArrayList<String> pupils = new ArrayList<String>();
+		pupils.add("Martina");
 		pupils.add("Leo");
 		pupils.add("Martin");
-		pupils.add("Feder");
+		pupils.add("Fedor");
 		pupils.add("David");
 		pupils.add("Oleg");
 		pupils.add("Chemmy");
 		pupils.add("Viktor");
 		pupils.add("Basti");
 
-		Collections.sort(pupils);
+		for (int j = 0; j < pupils.size() - 1; j++) {
+			for (int i = 0; i < pupils.size() - 1; ++i) {
+				if (pupils.get(i).compareTo(pupils.get(i + 1)) > 0) {
 
-		for (String i : pupils) {
-			System.out.println(i);
+					String temporary = pupils.get(i);
+					pupils.set(i, pupils.get(i + 1));
+					pupils.set(i + 1, temporary);
+				}
+			}
 		}
+		System.out.print(pupils);
 	}
+
 }
